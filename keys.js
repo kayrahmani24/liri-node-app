802795f79d6242ccaa02693608c79710e8a5771b
Kayvan Rahmani <Rahmani.kay1@gmail.com>
2017-11-09 13:48:17 -0600

    Setup keys and files

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+consumer_key: 'WSZcPt53xZjYlHLXGWM04dLbF',
+  consumer_secret: 'F9aki4z2J78iUqsATFlRFbsOCX1VTQRbMXVJBpvdq0Rx77ghXc',
+  access_token_key: '928359426456768512-BofCXLan9l9bgFfzrhh2KmyFUMYq6d9',
+  access_token_secret: 'UqQOzaQejwzLlGPEf5TJN4KadA2oLMSFgXQrobMCW1LQZ',
+}
+
+module.exports = twitterKeys;
+
+
