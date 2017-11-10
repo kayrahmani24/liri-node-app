8de3c67d97738fefb518534d50fb526b4caba659
Kayvan Rahmani <Rahmani.kay1@gmail.com>
2017-11-10 10:16:58 -0600

    twitter npm working

--- keys.js
+++ keys.js
@@ -1,12 +1,25 @@
-console.log('this is loaded');
+
 
 var twitterKeys = {
 consumer_key: 'WSZcPt53xZjYlHLXGWM04dLbF',
   consumer_secret: 'F9aki4z2J78iUqsATFlRFbsOCX1VTQRbMXVJBpvdq0Rx77ghXc',
   access_token_key: '928359426456768512-BofCXLan9l9bgFfzrhh2KmyFUMYq6d9',
-  access_token_secret: 'UqQOzaQejwzLlGPEf5TJN4KadA2oLMSFgXQrobMCW1LQZ',
+  access_token_secret: 'UqQOzaQejwzLlGPEf5TJN4KadA2oLMSFgXQrobMCW1LQZ'
+};
+
+var spotifyKeys = {
+  id: '3d76a7a3ec1e4e8aba940c9313e764cb',
+  secret: '629f705662f94251b8c656c19a1c1877'
+
+};
+
+
+
+
+module.exports = { 
+	twitterKeys: twitterKeys,
+	spotifyKeys: spotifyKeys
+
+
 }
-
-module.exports = twitterKeys;
-
 
--- liri.js
+++ liri.js
@@ -1,1 +1,22 @@
-var twitterKeys = require('twitterKeys');
+var keys = require("./keys.js");
+
+var twitterKeys = keys.twitterKeys;
+var spotifyKeys = keys.spotifyKeys;
+
+var Twitter = require('twitter');
+
+var client = new Twitter(twitterKeys);
+
+var nodeCommand = process.argv[2];
+var nodeArg = process.argv[3];
+var userName = 'KevinArmani3';
+
+var params = {screen_name: userName};
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+     
+    console.log(response);
+   
+}
+});
